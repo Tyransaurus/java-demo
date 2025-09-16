49a199896ab617a7b770e88ea86f2017c31b5dec
Dick Jiang <djjiang708405@gmail.com>
2025-09-16 17:35:37 -0400

    initialized the home controller and the template which is the index.html file to use it. Thymeleaf uses this as its a template engine

--- src/main/java/com/demo/java_demo/HomeController.java
+++ src/main/java/com/demo/java_demo/HomeController.java
@@ -1,7 +1,17 @@
 package com.demo.java_demo;
 
-import org.
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;;;
 
-public class HomeController {
-
+@Controller
+public class HomeController 
+{
+    @GetMapping("/")
+    public String index(Model model)
+    {
+        model.addAttribute("title", "DJ's Java Demo");
+        model.addAttribute("message", "Hello from Spring Boot!");
+        return ("index"); // this goes to look for templates 
+    }
 }
