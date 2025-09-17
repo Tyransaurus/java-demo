2621cf114ce852730b1dd4607cf1d3e12127c8a9
Dick Jiang <djjiang708405@gmail.com>
2025-09-16 20:49:44 -0400

    updated index to have a banner that grabs it from the aws

--- src/main/java/com/demo/java_demo/HomeController.java
+++ src/main/java/com/demo/java_demo/HomeController.java
@@ -2,18 +2,33 @@ package com.demo.java_demo;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;;;
+import org.springframework.web.bind.annotation.*;
+;;
 
 @Controller
 public class HomeController 
 {
 
-    
+    private final BannerService bannerService;
+
+    public HomeController(BannerService bannerService) {
+        this.bannerService = bannerService;
+    }
+
     @GetMapping("/")
     public String index(Model model)
     {
         model.addAttribute("title", "DJ's Java Demo");
         model.addAttribute("message", "Hello from Spring Boot!");
+        model.addAttribute("banner", bannerService.readBanner());
         return ("index"); // this goes to look for templates 
     }
+
+    @PostMapping("/admin/banner")
+    public String updateBanner(@RequestParam String text)
+    {
+        bannerService.writeBanner(text);
+        return "redirect:/";
+    }
+    
 }
